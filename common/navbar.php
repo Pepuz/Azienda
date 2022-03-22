6ad63e56b601c1ad7720cf483268e0dc9fee570f
Nikoo93 <98648723+Nikoo93@users.noreply.github.com>
2022-03-22 21:54:15 +0100

    Update navbar.php

--- common/navbar.php
+++ common/navbar.php
@@ -11,7 +11,7 @@
                     <li class="active"><a class="active" href="index.php?op=riunioni">Lista Riunioni</a></li>
                 </ul>
                 <?php
-                if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore') {
+                if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore' || (isset($_SESSION['data_autorizzazione']))) {
                     echo "<ul aria-expanded=\"false\"><li class=\"active\"><a class=\"active\" href=\"index.php?op=riunioni\">Crea Riunione</a></li></ul>";
                 }
                 ?>
