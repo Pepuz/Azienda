a03f2aa21836c6eb4eaeeadaa610d7d73a7021e3
Nikoo93 <98648723+Nikoo93@users.noreply.github.com>
2022-03-22 10:15:57 +0100

    Update functions.php

--- common/functions.php
+++ common/functions.php
@@ -1,1 +1,11 @@
+<?php
+
+function isUser($cid,$email,$pwd)
+{
+  $query = "SELECT * FROM utenti WHERE email='$email' and password='$pwd'";
+  $result = $cid->query($query);
+  
+  return $result;
+}
+  
 
