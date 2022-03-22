a68e7936d52eca442c1368671969cc69d5bf4474
Nikoo93 <98648723+Nikoo93@users.noreply.github.com>
2022-03-22 10:29:04 +0100

    Update functions.php

--- common/functions.php
+++ common/functions.php
@@ -7,5 +7,19 @@ function isUser($cid,$email,$pwd)
   
   return $result;
 }
+
+function nextMeeting($cid,$email)
+{
+  $query = "SELECT * FROM partecipa JOIN riunioni 
+  WHERE riunione=id AND partecipante= '$email'
+  AND Data > CURDATE()
+  OR (Data = CURDATE() AND Ora > TIME(NOW()))
+  ORDER BY Data ASC, Ora ASC 
+  LIMIT 1";
+  
+  $result = $cid->query($query);
+  
+  return $result;
+}
   
 
