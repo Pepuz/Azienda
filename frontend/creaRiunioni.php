892487a597af4891691c1e1c57732d14e013f3f6
Nikoo93 <98648723+Nikoo93@users.noreply.github.com>
2022-04-05 15:18:47 +0200

    Create creaRiunioni.php

---
+++ frontend/creaRiunioni.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Custom Select</h4>
+                                <div class="basic-form">
+                                    <form>
+                                        <div class="form-row align-items-center">
+                                            <div class="col-auto my-1">
+                                                <label class="mr-sm-2">Preference</label>
+                                                <select class="custom-select mr-sm-2" name="sale" id="sale">
+                                                    <option selected="selected">Seleziona Dipartimento</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+  </div>
