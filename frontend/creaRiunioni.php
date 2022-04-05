76959a922ca3dfff53982a8005bf010d3a5211ae
Nikoo93 <98648723+Nikoo93@users.noreply.github.com>
2022-04-05 15:33:42 +0200

    Update creaRiunioni.php

--- frontend/creaRiunioni.php
+++ frontend/creaRiunioni.php
@@ -10,12 +10,10 @@
                                     <form>
                                         <div class="form-row align-items-center">
                                             <div class="col-auto my-1">
-                                                <label class="mr-sm-2">Preference</label>
-                                                <select class="custom-select mr-sm-2" name="sale" id="sale">
+                                                <label class="mr-sm-2">Dipartimento</label>
+                                                <select class="custom-select mr-sm-2" name="dipartimento" id="dipartimento">
                                                     <option selected="selected">Seleziona Dipartimento</option>
                                                     <option value="1">One</option>
-                                                    <option value="2">Two</option>
-                                                    <option value="3">Three</option>
                                                 </select>
                                             </div>
                                         </div>
