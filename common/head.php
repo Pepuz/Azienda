0801570e281e1c283e7c88007fba18011a5b7bf7
Nikoo93 <98648723+Nikoo93@users.noreply.github.com>
2022-06-07 10:39:09 +0200

    Add files via upload

--- common/head.php
+++ common/head.php
@@ -7,5 +7,12 @@
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
     
     <link href="css/style.css" rel="stylesheet">
+	<link href="plugins/animate/animate.min.css" rel="stylesheet">
+	<link href="plugins/metismenu/css/metisMenu.min.css" rel="stylesheet">
+	<link href="plugins/bootstrap-datepicker/bootstrap-datepicker.min.css" rel="stylesheet">
+	<link href="plugins/jquery-timepicker/css/jquery.timepicker.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://unpkg.com/multiple-select@1.5.2/dist/multiple-select.min.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+	<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
 
 </head>
