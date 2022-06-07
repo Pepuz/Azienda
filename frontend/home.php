dbcfa3b5aa5a4d00c06743b9705ee48759291e4e
Pepuz <96133096+Pepuz@users.noreply.github.com>
2022-06-07 17:07:58 +0200

    Update home.php

--- frontend/home.php
+++ frontend/home.php
@@ -9,12 +9,46 @@ $row = $result->fetch_assoc();
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-lg-12">
-						<div class="card-body">
-							<h1><?php echo 'Benvenuto,' . " " . $_SESSION['nome'] . " " . $_SESSION['cognome']; ?></h1>
+					<div class="card-body">
+						<h1><?php echo 'Benvenuto,' . " " . $_SESSION['nome'] . " " . $_SESSION['cognome']; ?></h1>
+					</div>
+					<div class="col-lg-12">
+						<div class="card">
+							<div class="card-body">
+								<div class="card-title">
+									<h4>Prossima Riunione</h4>
+								</div>
+								<div class="table-responsive">
+									<table class="table table-striped">
+										<?php
+										if ($count > 0) {
+											echo "<thead>
+							<tr>
+								<th>Tema</th>
+								<th>Sala</th>
+								<th>Data</th>
+								<th>Inizio</th>
+								<th>Fine</th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+                                <td>" . $row['tema'] . "</td>
+                                    <td>" . $row['salariunioni'] . "</td>
+                                    <td>" . $row['data_riunione'] . "</td>
+                                    <td>" . $row['ora'] . "</td>
+									<td>" . $row['durata'] . "</td>
+                            </tr>
+                        </tbody>";
+										} else {
+											echo "<tr> <td colspan='2'> Nessuna riunione programmata per l'utente </td></tr>";
+										}
+										?>
+									</table>
+								</div>
+							</div>
 						</div>
-						<?php
-							include 'table-content.php';
-						?>
+					</div>
 				</div>
 			</div>
 		</div>
