77d37d1f3fde5a288ca13997f71a64287983a1f6
Pepuz <96133096+Pepuz@users.noreply.github.com>
2022-06-07 11:10:25 +0200

    Update navbar.php

--- common/navbar.php
+++ common/navbar.php
@@ -12,29 +12,17 @@
                 
                 <?php
                 if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore' || (isset($_SESSION['data_autorizzazione']))) {
-                    echo "<li class=\"active\"><a href=\"index.php?op=creaRiunioni\">Crea Riunione</a></li>";
-                }
-                ?>
-				<?php
-                if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore' || (isset($_SESSION['data_autorizzazione']))) {
-                    echo "<li class=\"active\"><a href=\"index.php?op=formModifica\">Modifica Riunione</a></li>";
-                }
-                ?>
-				<?php
-                if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore' || (isset($_SESSION['data_autorizzazione']))) {
-                    echo "<li class=\"active\"><a href=\"index.php?op=eliminaRiunioni\">Elimina Riunioni</a></li>";
+                    echo "<li class=\"active\"><a href=\"index.php?op=creaRiunioni\">Crea Riunione</a></li>
+                        <li class=\"active\"><a href=\"index.php?op=formModifica\">Modifica Riunione</a></li>
+                        <li class=\"active\"><a href=\"index.php?op=eliminaRiunioni\">Elimina Riunioni</a></li>";
                 }
                 ?>
 				</ul>
             </li>
             <?php
 			if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore') {
-				echo "<li><a href=\"index.php?op=registrazione\" aria-expanded=\"false\"><i class=\"icon-badge menu-icon\"></i><span class=\"nav-text\">Registrazioni</span></a></li>";
-			}
-			?>
-            <?php
-			if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore') {
-				echo "<li><a href=\"index.php?op=autorizzazioni\" aria-expanded=\"false\"><i class=\"icon-badge menu-icon\"></i><span class=\"nav-text\">Autorizzazioni</span></a></li>";
+				echo "<li><a href=\"index.php?op=registrazione\" aria-expanded=\"false\"><i class=\"icon-badge menu-icon\"></i><span class=\"nav-text\">Registrazioni</span></a></li>
+                    <li><a href=\"index.php?op=autorizzazioni\" aria-expanded=\"false\"><i class=\"icon-badge menu-icon\"></i><span class=\"nav-text\">Autorizzazioni</span></a></li>";
 			}
 			?>
             <li>
