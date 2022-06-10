5619dac3427cdb71ad7dd62fb7d37bb3bb6a0810
Nikoo93 <98648723+Nikoo93@users.noreply.github.com>
2022-06-10 21:47:18 +0200

    Add files via upload

--- frontend/modifica-profilo.php
+++ frontend/modifica-profilo.php
@@ -1,10 +1,37 @@
 <?php
-if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] != 'direttore') {
-	redirect("frontend/page-error.html");
-}
+session_start();
 
 ?>
 
+<!DOCTYPE html>
+<html class="h-100" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Modifica email e password</title>
+    <link rel="icon" type="image/png" href="../images/favicon.png">
+    <link href="../css/style.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+
+</head>
+
+<body class="h-100">
+
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="loader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+            </svg>
+        </div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
 <script>
 $( function() {
 	$('#password, #confirm_password').on('keyup', function () {
@@ -26,93 +53,55 @@ $(function () {
         return true;
     });
 });
-	
-$(function () {
-    $('#registrazione').submit(function (e) {
-		e.preventDefault();
-		$.ajax({
-			type: 'POST',
-			url: 'backend/check_registrazione.php',
-			data: $('#registrazione').serialize(),
-			success: function (data) {
-				var json = $.parseJSON(data);
-
-				if(json.status=== 'error'){
-					$("#error").html(json.errors[0]);
-
-					return false;
-				}
-				if(json.status==='success'){
-					localStorage.setItem('alert',true);
-					window.location.reload();
-			
-				}
-			}
-		});
-    });
-});
-
-$( function () {
-		if(localStorage.getItem("alert")){
-			localStorage.removeItem("alert");
-            $('#successo').html("Dati modificati correttamente"); 
-			$("#successo").show();
-			$('#successo').delay(3000).fadeOut('slow');
-        }
-    } 
-);  
-
-$(document).ready(function(){
-    $(window).scrollTop(0);
-});
 </script>
+    <div class="login-form-bg h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100">
+                <div class="col-xl-6">
+                    <div class="form-input-content">
+                        <div class="card login-form mb-0">
+                            <div class="card-body pt-5">
+                                <a class="text-center">
+                                    <h4>Modifica password</h4>
+                                </a>
 
-<div class="content-body">
-	<div class="row page-titles mx-0">
-	<div class="alert alert-success" id="successo" style="display:none;"></div>
-		<div class="col-lg-12">   
-			<div class="login-form-bg h-100">
-				<div class="container h-100">
-					<div class="row justify-content-center h-100">
-						<div class="col-xl-6">
-							<div class="form-input-content">
-								<div class="card login-form mb-0">
-									<div class="card-body pt-5">
-										
-											<h4>Modifica profilo</h4>
-				
-										<form autocomplete="off" class="mt-5 mb-5 login-input" id="registrazione">
-                                        
-											
-											<div class="form-group">
-												<input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
-											</div>
-											
-											
-											
-											<div class="form-group">
-												<input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-											</div>
-											<div class="form-group">
-												<input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Conferma Password" required>
-												<span id='message'></span>
-											</div>
-											<button type="submit" id="submit" class="btn login-form__btn submit w-100">Conferma modifiche</button>
-											<span id="error" style='color:red;'></span>
-										</form>
+                                <form method="POST" class="mt-5 mb-5 login-input" action="../backend/check_profile.php">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" placeholder="Email" name="email" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" id="password" placeholder="Nuova Password" name="new_password" required>
+                                    </div>
+									<div class="form-group">
+										<input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Conferma Password" required>
+										<span id='message'></span>
 									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                                    <div></div>
+                                    <button type="submit" class="btn login-form__btn submit w-100" style="margin-bottom: 20px;">Modifica</button>
+									<?php
+                                    if (isset($_SESSION["error_pass"])) {
+                                        $error = $_SESSION["error_pass"];
+                                        echo "<span style='color:red;'>$error</span>";
+                                    }
+                                    ?>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
 
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <script src="../plugins/common/common.min.js"></script>
+    <script src="../js/custom.min.js"></script>
 
+</body>
 
+</html>
