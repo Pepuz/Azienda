9406ad138e24384b98d32fbb1a17eeffdfed457b
Nikoo93 <98648723+Nikoo93@users.noreply.github.com>
2022-06-10 20:01:03 +0200

    Add files via upload

--- common/navbar.php
+++ common/navbar.php
@@ -5,11 +5,11 @@
             <hr class="my-2">
             <li class="mega-menu mega-menu-sm">
                 <a class="has-arrow" href="javascript:void()" aria-expanded="false">
-                    <i class="icon-notebook menu-icon"></i><span class="nav-text">Riunioni</span>
+                    <span class="nav-text">Riunioni</span>
                 </a>
                 <ul aria-expanded="false">
-                    <li class="active"><a href="index.php?op=riunioni">Riunioni Programmate</a></li>
-                    <li class="active"><a href="index.php?op=passate">Riunioni Passate</a></li>
+                    <li class="active"><a href="index.php?op=riunioni">Lista Riunioni</a></li>
+                
                 <?php
                 if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore' || (isset($_SESSION['data_autorizzazione']))) {
                     echo "<li class=\"active\"><a href=\"index.php?op=creaRiunioni\">Crea Riunione</a></li>
@@ -21,13 +21,12 @@
             </li>
             <?php
 			if (isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'direttore') {
-				echo "<li><a href=\"index.php?op=registrazione\" aria-expanded=\"false\"><i class=\"icon-badge menu-icon\"></i><span class=\"nav-text\">Registrazioni</span></a></li>
-                    <li><a href=\"index.php?op=autorizzazioni\" aria-expanded=\"false\"><i class=\"icon-badge menu-icon\"></i><span class=\"nav-text\">Autorizzazioni</span></a></li>";
+				echo "<li><a href=\"index.php?op=autorizzazioni\" aria-expanded=\"false\"></i><span class=\"nav-text\">Autorizzazioni</span></a></li>";
 			}
 			?>
             <li>
                 <a href="index.php?op=statistiche" aria-expanded="false">
-                    <i class="icon-badge menu-icon"></i><span class="nav-text">Statistiche</span>
+                    <span class="nav-text">Statistiche</span>
                 </a>
             </li>
         </ul>
